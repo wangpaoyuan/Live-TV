7ed54e4ef02dcf9331c4658d4990672161500b48
yiwo <134620533+wangpaoyuan@users.noreply.github.com>
2024-03-28 08:44:32 +0800

    Add files via upload

---
+++ verify/svi_auth.php
@@ -0,0 +1,10 @@
+<?php
+
+//echo '{"result_code":"0","result":"ok","keys":{"user_id":"00270000000006be","peer_id":"01aa62b1b3ccd771","session_key":"09bf0bdb5c6688d2"}}';
+//echo '{"result_code":"0","result":"ok","keys":{"user_id":"00270000000006be","peer_id":"01aa62b1b3cccbd7","session_key":"3838587c708dd95b"}}';
+
+//echo '{"result_code":"0","result":"ok","keys":{"user_id":"002700000005213c","peer_id":"679c72782b2a3673","session_key":"26ac810b8524a763"}}';
+
+echo '{"keys":{"peer_id":"01aa4581c6ee34ab","session_key":"53d8fac31d7d3531","user_id":"00270000000004d0"},"result":"ok","result_code":"0"}';
+
+?>
